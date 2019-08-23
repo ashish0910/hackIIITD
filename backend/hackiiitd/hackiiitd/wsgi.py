641cbfc42f8c050bc30194128ffcf04e5d2e9bfa
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-08-23 20:22:34 +0530

    Renamed project names

--- backend/hackiiitd/hackiiitd/wsgi.py
+++ backend/hackiiitd/hackiiitd/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hackdelhi project.
+WSGI config for hackiiitd project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackdelhi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackiiitd.settings")
 
 application = get_wsgi_application()
